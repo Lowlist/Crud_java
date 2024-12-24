9973c73ca6f7071c1b5172559af2b4073ee5c261
dud5825@naver.com <dud5825@naver.com>
2024-12-24 16:39:06 +0900

    [조영민] 2024-12-24 -기본세팅 완료 SpringBoot 같은경우엔 web.xml + root-context.xml + servlet-context.xml 를 application.yml로 대체함

---
+++ Crud_study/src/main/java/com/example/crud/board/controller/mainController.java
@@ -0,0 +1,15 @@
+package com.example.crud.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class mainController {
+	
+	@GetMapping("/")
+	public String main() {
+		return "index";
+	}
+}
---
+++ Crud_study/src/main/webapp/WEB-INF/view/index.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Crud테스트용</title>
+</head>
+<body>
+	<div class="main">
+		<div id=""></div>
+	</div>
+</body>
+</html>
